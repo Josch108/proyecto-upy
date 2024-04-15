68c44a5e606ce351e38e1a518cf43c7b981b42a6
Josch108 <166523087+Josch108@users.noreply.github.com>
2024-04-14 19:38:32 -0600

    numeric base converter.py

--- numeric base converter.py
+++ numeric base converter.py
@@ -1,49 +1,36 @@
-
-# binary to decimal
 def binary_to_decimal(binary):
     return int(binary, 2)
 
-# binary to octal
 def binary_to_octal(binary):
     return oct(int(binary, 2))[2:]
 
-# binary to hexadecimal
 def binary_to_hexadecimal(binary):
     return hex(int(binary, 2))[2:].upper()
 
-# decimal to binary
 def decimal_to_binary(decimal):
     return bin(decimal)[2:]
 
-# decimal to octal
 def decimal_to_octal(decimal):
     return oct(decimal)[2:]
 
-# decimal to hexadecimal
 def decimal_to_hexadecimal(decimal):
     return hex(decimal)[2:].upper()
 
-# octal to binary
 def octal_to_binary(octal):
     return bin(int(octal, 8))[2:]
 
-# octal to decimal
 def octal_to_decimal(octal):
     return int(octal, 8)
 
-# octal to hexadecimal
 def octal_to_hexadecimal(octal):
     return hex(int(octal, 8))[2:].upper()
 
-# hexadecimal to binary
 def hexadecimal_to_binary(hexadecimal):
     return bin(int(hexadecimal, 16))[2:]
 
-# hexadecimal to decimal
 def hexadecimal_to_decimal(hexadecimal):
     return int(hexadecimal, 16)
 
-# hexadecimal to octal
 def hexadecimal_to_octal(hexadecimal):
     return oct(int(hexadecimal, 16))[2:]
 
